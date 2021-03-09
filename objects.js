a8f9a503c6e02bd10c854c21ccc6d7bc9a1a444b
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-09 07:27:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {"artistName" : "songTitle"}
 function updatePlaylist(playlist,artistName,songTitle){
   return playlist[artistName]=songTitle
 }
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+  return playlist
+}
