08750d0f2e8ddbde324f355aa1035ec40703c609
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-09 07:25:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 var playlist = {"artistName" : "songTitle"}
 
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist.assign(playlist,{artistName:songTitle})
-  return playlist
+  return playlist.assign(playlist,{artistName:songTitle})
 }
